1e85fe36548d8acb3c91709eaaca0186e3e26125
juysoft <package@juysoft.com>
2016-09-18 16:02:22 +0300

    Remove compareDotArrays() completely and use laravel str_is

--- src/Active.php
+++ src/Active.php
@@ -32,75 +32,22 @@ class Active
      */
     public function route($route, $class = 'active')
     {
+        // Convert to array
         if (!is_array($route))
         {
             $route = explode(' ', $route);
         }
 
-        $match = false;
-
-        foreach ($route as $value)
+        // Check the current route name
+        // https://laravel.com/docs/5.3/helpers#method-str-is
+        foreach ((array) $route as $i)
         {
-            if ($this->compareDotArrays($value, $this->currentRouteName))
+            if (str_is($i, $this->currentRouteName))
             {
-                $match = true;
-                break;
+                //return true;
+                return $class;
             }
         }
-
-        return $match ? $class : '';
     }
 
-    /**
-     * Compare two dot notation values. Accepts '*' as wildcard
-     *
-     * @param $dot1
-     * @param $dot2
-     *
-     * @return bool
-     */
-    protected function compareDotArrays($dot1, $dot2)
-    {
-        $array1 = explode('.', $dot1);
-        $array2 = explode('.', $dot2);
-
-        if (count($array1) > count($array2))
-        {
-            $count = count($array1);
-        }
-        else
-        {
-            $count = count($array2);
-        }
-
-        $match = true;
-
-        for ($i = 0; $i < $count; $i++)
-        {
-            if (!isset($array2[$i]))
-            {
-                if ($array1[$i] !== '*')
-                {
-                    $match = false;
-                }
-                break;
-            }
-
-            if (!isset($array1[$i]))
-            {
-                if ($array1[$i - 1] !== '*')
-                {
-                    $match = false;
-                }
-                break;
-            }
-
-            if ($array1[$i] !== $array2[$i] && $array1[$i] !== '*')
-            {
-                $match = false;
-            }
-        }
-
-        return $match;
-    }
 }
