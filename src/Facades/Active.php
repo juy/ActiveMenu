3cb802c8ed34db8ebd08ea39e85c41bb801b7527
juysoft <package@juysoft.com>
2016-12-15 12:27:29 +0200

    Add docblock to Facade

--- src/Facades/Active.php
+++ src/Facades/Active.php
@@ -20,6 +20,11 @@ use Illuminate\Support\Facades\Facade;
  */
 class Active extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'active';
